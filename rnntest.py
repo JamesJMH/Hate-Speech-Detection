3cd27d5d509415f08f1a9d2185098de678cb25e5
JamesJMH <jh7zr@virginia.edu>
2018-08-13 14:26:03 -0400

    2 Class high f1 for "none"
    adam, 200 hidden nodes

--- rnntest.py
+++ rnntest.py
@@ -18,7 +18,8 @@ from keras.layers import Flatten
 from keras.layers import TimeDistributed
 from keras.optimizers import RMSprop
 import convert
-
+from string import punctuation
+from gensim.parsing.preprocessing import STOPWORDS
 
 # with open("glove.6B.50d.txt", "rb") as lines:
 #     w2v = {line.split()[0]: np.array(map(float, line.split()[1:])) for line in lines}
@@ -54,133 +55,6 @@ def min_indicator(x):
 # In[96]:
 
 
-# class MeanEmbeddingVectorizer(object):
-#     def __init__(self, word2vec):
-#         self.word2vec = word2vec
-#         # if a text is empty we should return a vector of zeros
-#         # with the same dimensionality as all the other vectors
-#         self.dim = len(word2vec.itervalues().next())
-#
-#     def fit(self, X, y):
-#         return self
-#
-#     # change to return x3
-#     # column that has the most maxes
-#     # column that has the most min
-#     def transform(self, X):
-#         return np.array([
-#                             np.concatenate([
-#
-#                                 # mean of vectors
-#                                 np.mean([self.word2vec[w] for w in words if w in self.word2vec],
-#                                         # or [np.zeros(self.dim)],
-#                                         axis=0),
-#
-#                                 # min of vectors
-#                                 np.min([self.word2vec[w] for w in words if w in self.word2vec],
-#                                        # or [np.zeros(self.dim)],
-#                                        axis=0),
-#
-#                                 # max of vectors
-#                                 np.max([self.word2vec[w] for w in words if w in self.word2vec],
-#                                        # or [np.zeros(self.dim)],
-#                                        axis=0),
-#
-#                                 # max of dimmensions
-#                                 np.mean([max_indicator(self.word2vec[w]) for w in words if w in self.word2vec]
-#                                         # or [np.zeros(self.dim)]
-#                                         , axis=0),
-#                                 # is currently returning the word's index (where the word has the maximum)
-#
-#                                 # min of dimmensions
-#                                 np.mean([min_indicator(self.word2vec[w]) for w in words if w in self.word2vec]
-#                                         # or [np.zeros(self.dim)]
-#                                         , axis=0)],
-#
-#                             )
-#                             for words in X
-#
-#                             # np.mean([self.word2vec[w] for w in words if w in self.word2vec]
-#                             #        or [np.zeros(self.dim)], axis=0)
-#                             # for words in X
-#                             ])
-#
-#
-# # In[97]:
-#
-#
-#
-# class TfidfEmbeddingVectorizer(object):
-#     def __init__(self, word2vec):
-#         self.word2vec = word2vec
-#         self.word2weight = None
-#         #cur=next(iter(word2vec.values()))
-#         self.dim = len(next(iter(word2vec.values())))
-#
-#     def fit(self, X, y):
-#         tfidf = TfidfVectorizer(analyzer=lambda x: x)
-#         tfidf.fit(X)
-#         # if a word was never seen - it must be at least as infrequent
-#         # as any of the known words - so the default idf is the max of
-#         # known idf's
-#         max_idf = max(tfidf.idf_)
-#         self.word2weight = defaultdict(
-#             lambda: max_idf,
-#             [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
-#
-#         return self
-#
-#     def transform(self, X):
-#         return np.array([
-#                             np.concatenate([
-#
-#                                 # mean of vectors
-#                                 np.mean([self.word2vec[w] * self.word2weight[w]
-#                                          for w in words if w in self.word2vec], axis=0),
-#
-#                                 # min of vectors
-#                                 np.min([self.word2vec[w] * self.word2weight[w]
-#                                         for w in words if w in self.word2vec], axis=0),
-#
-#                                 # max of vectors
-#                                 np.max([self.word2vec[w] * self.word2weight[w]
-#                                         for w in words if w in self.word2vec], axis=0),
-#
-#                                 # max of dimmensions
-#                                 np.mean([self.word2vec[w] * self.word2weight[w]
-#                                          for w in words if w in self.word2vec], axis=0),
-#                                 # is currently returning the word's index (where the word has the maximum)
-#
-#                                 # min of dimmensions
-#                                 np.mean([self.word2vec[w] * self.word2weight[w]
-#                                          for w in words if w in self.word2vec], axis=0)],
-#
-#                             )
-#                             for words in X
-#
-#                             # np.mean([self.word2vec[w] for w in words if w in self.word2vec]
-#                             #        or [np.zeros(self.dim)], axis=0)
-#                             # for words in X
-#                             ])
-
-
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
-
-# '''
-# etree_w2v = Pipeline([
-#     ("word2vec vectorizer", MeanEmbeddingVectorizer(w2v)),
-#     ("extra trees", ExtraTreesClassifier(n_estimators=200))]) #extratrees is from sklearn; try randomforest or gradient boosted
-# '''
-# etree_w2v = Pipeline([
-#     ("word2vec vectorizer", MeanEmbeddingVectorizer(w2v)),
-#     ("random forest",
-#      RandomForestClassifier(n_estimators=200))])  # extratrees is from sklearn; try randomforest or gradient boosted
-#
-# etree_w2v_tfidf = Pipeline([
-#     ("word2vec vectorizer", TfidfEmbeddingVectorizer(w2v)),
-#     ("extra trees", SVC())])  # you can use 1000
-
 
 import convert, string
 import emoji
@@ -189,85 +63,42 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import defaultdict
 
 
-def strip(text):
-    exclude = set(string.punctuation)
-    text = ''.join(c for c in text if c not in exclude)
-    return ''.join(c for c in text if c not in emoji.UNICODE_EMOJI)
 
 
 X_text = []
 X=[]
 Y = []
-# with open("hate_data.csv","rU") as f:
-#     readCSV = csv.reader(f, delimiter=',')
-#     for row in readCSV:
-#         tweet=strip(row[19]) #row[6]
-#         label=(row[5]) #row[5]
-#         tweet = tweet.lower().decode('utf-8','ignore').encode("utf-8")
-#         label = label.encode('utf-8')
-#         for word in tweet.split():
-#             if (word in w2v):
-#                 X.append(tweet.split())
-#                 if label=="The tweet uses offensive language but not hate speech":
-#                     Y.append(1)
-#                 elif label=="The tweet contains hate speech":
-#                     Y.append(2)
-#                 else:
-#                     Y.append(0)
-#                 break
 
-# with open("labeled_data.csv","rU") as f:
-#     readCSV = csv.reader(f, delimiter=',')
-#     for row in readCSV:
-#         tweet=strip(row[6]) #row[6]
-#         label=(row[5]) #row[5]
-#         tweet = tweet.lower()
-#         label = label.encode('utf-8')
-#         for word in tweet.split():
-#             X_text.append(tweet.split())
-#                 # if label=="The tweet uses offensive language but not hate speech":
-#                 #     Y.append(1)
-#             if label=="0":
-#                 Y.append('1')
-#                 # elif label=="1":
-#                 #     Y.append(1)
-#             else:
-#                 Y.append('0')
-#             # Y.append(label)
-#
-#             break
 
 
 with open("wassem_hovy_naacl.csv", encoding="utf-8")as f:
     readCSV = csv.reader(f, delimiter="\t")
     for row in readCSV:
-        tweet=strip(row[3]) #row[6]
+        tweet=row[3] #row[6]
         label=(row[4]) #row[5]
         tweet = tweet.lower()
         # label = label.encode('utf-8')
-        for word in tweet.split():
-            X_text.append(tweet.split())
-                # if label=="The tweet uses offensive language but not hate speech":
-                #     Y.append(1)
-            # if label=="none":
-            #     Y.append(0)
-            #     # elif label=="1":
-            #     #     Y.append(1)
-            # else:
-            #     Y.append(1)
+        splitted=tweet.split()
+        to_append=[]
+        for tw in splitted:
+            if tw not in punctuation and tw not in STOPWORDS and "@" not in tw:
+                to_append.append(tw)
+        X_text.append(to_append)
+        # if label=="none":
+        #     Y.append([1, 0,0])
+        # elif label=="racism":
+        #     Y.append([0,1,0])
+        #
+        # else:
+        #     Y.append([0,0,1])
 
-            if label=="none":
-                Y.append([1, 0,0])
-
-            elif label=="racism":
-                Y.append([0,1,0])
-
-            else:
-                Y.append([0,0,1])
-
-            break
+        if label=="none":
+            Y.append(0)
+        else:
+            Y.append(1)
 
 
+# using pretrained glove file
 X_text=X_text[1:]
 test_tweets=X_text[:200]
 for tweet in X_text:
@@ -276,6 +107,11 @@ for tweet in X_text:
         if words in w2v:
             cur.append(w2v[words])
     X.append(cur)
+
+
+
+
+
 # from sklearn.feature_extraction.text import TfidfVectorizer
 #
 # tokenize = lambda doc: doc.lower().split(" ")
@@ -326,18 +162,19 @@ Y=Y[200:]
 
 
 
-timesteps=31 # number of words in a sentence
+timesteps=maxlen # number of words in a sentence
 input_dim=100 # dimension of embedding
 features=100
 
 
-hidden_size=50
+hidden_size=200
 # tokenize and build vocab
 model = Sequential()
 model.add(LSTM(hidden_size, return_sequences=True, input_shape=(timesteps, input_dim)))
 model.add(Dropout(0.5))
+# model.add(LSTM(hidden_size))
 model.add(Flatten())
-model.add(Dense(3,activation='sigmoid'))
+model.add(Dense(1,activation='sigmoid'))
 
 
 # model = Sequential()
@@ -347,9 +184,9 @@ model.add(Dense(3,activation='sigmoid'))
 # model.add(TimeDistributed(Dense(len(w2v))))
 # model.add(Activation('softmax'))
 
-optim=RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optim, metrics=['accuracy'])
-model.fit(X, Y, epochs=5)
+optim=RMSprop(lr=0.001)
+model.compile(loss='binary_crossentropy', optimizer="adam", metrics=['accuracy'])
+model.fit(X, Y, epochs=8)
 
 
 
@@ -387,16 +224,15 @@ for i in range(len(preds)):
 print(model.evaluate(Xtest, Ytest, verbose=0))
 
 from sklearn.metrics import precision_recall_fscore_support
-ytrue=[]
-ypred=[]
-for y in Ytest:
-    print(y)
-    if y[0]==1:
-        ytrue.append(0)
-    elif y[1]==1:
-        ytrue.append(1)
-    else:
-        ytrue.append(2)
+# ytrue=[]
+# ypred=[]
+# for y in Ytest:
+#     if y[0]==1:
+#         ytrue.append(0)
+#     elif y[1]==1:
+#         ytrue.append(1)
+#     else:
+#         ytrue.append(2)
 
 
-print(precision_recall_fscore_support(ytrue, preds))
+print(precision_recall_fscore_support(Ytest, preds))
